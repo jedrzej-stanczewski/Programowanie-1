b99c373a4e738d38ca9399446edfbd5e1886ae7c
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-10-14 18:24:10 +0200

    task 3

---
+++ s01-sub.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+
+int main(int argc, char *argv[])
+{
+  auto const a = std::stoi(argv[1]);
+  auto const b = std::stoi(argv[2]);
+  std ::cout << (a - b) << "\n";
+  return 0;
+}
