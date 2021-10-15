b54ec3a82f3e0c3c9d0900aeafce13f9774617ee
Jędrzej Stańczewski <91730807+jedrzej-stanczewski@users.noreply.github.com>
2021-10-15 13:10:40 +0200

    task 2 completed

--- tasks-02/s03-countdown.cpp
+++ tasks-02/s03-countdown.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
 
+auto main(int argc, char*argv[]) -> int {
+    int number = std::stoi(argv[1]);
+    if (number == 0) {
+        std::cout << number << std::endl;
+    }
+    else if (number > 0) {
+        for (auto i = 1; number > -1; --number) {
+            std::cout << number << std::endl;
+            }
+        }
+    else if (number < 0) {
+        for (auto i = 1; number < 1; ++number) {
+            std::cout << number << std::endl;
+            }
+        }
+    return 0;
+}
