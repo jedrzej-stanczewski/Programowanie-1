73806e38bfaf9077439373f788a2a576b44f9f2c
Jędrzej Stańczewski <91730807+jedrzej-stanczewski@users.noreply.github.com>
2021-10-22 12:23:09 +0200

    Task 6 completed

--- tasks-02/s03-beer.cpp
+++ tasks-02/s03-beer.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
 
+auto main(int argc, char*argv[]) -> int {
+    if (argv[1]) {
+        auto number = std::stoi(argv[1]);
+        int const original_number = number;
+        while (number > 1) {
+            std::cout << number << " bottles of beer on the wall, " << number << " bottles of beer." << std::endl << "Take one down and pass it around, " << (number - 1) << " bottles of beer on the wall." << std::endl;
+            --number;
+        }
+        if (number == 1) {
+            std::cout << "1 bottle of beer on the wall, 1 bottle of beer." << std::endl << "Take one down and pass it around, 0 bottles of beer on the wall." << std::endl;
+            -- number;
+        }
+        if (number == 0) {
+            std::cout << "No more bottles of beer on the wall, no more bottles of beer." << std::endl << "Go to the store and buy some more, "<< original_number << " bottles of beer on the wall." << std::endl;
+            }
+        }
+    else {
+        auto number = 99;
+        int const original_number = number;
+        while (number > 1) {
+            std::cout << number << " bottles of beer on the wall, " << number << " bottles of beer." << std::endl << "Take one down and pass it around, " << (number - 1) << " bottles of beer on the wall." << std::endl;
+            --number;
+        }
+        if (number == 1) {
+            std::cout << "1 bottle of beer on the wall, 1 bottle of beer." << std::endl << "Take one down and pass it around, 0 bottles of beer on the wall." << std::endl;
+            -- number;
+        }
+        if (number == 0) {
+            std::cout << "No more bottles of beer on the wall, no more bottles of beer." << std::endl << "Go to the store and buy some more, "<< original_number << " bottles of beer on the wall." << std::endl;
+        }
+    }
+    return 0;
+}
