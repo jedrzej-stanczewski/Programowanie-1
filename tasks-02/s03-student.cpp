9d5434a52ec4b7a5dcaf79645b2cfe1d103eed21
Jędrzej Stańczewski <jstanczewski99@gmail.com>
2021-10-25 20:23:36 +0200

    task 8 completed

--- tasks-02/s03-student.cpp
+++ tasks-02/s03-student.cpp
@@ -1,1 +1,25 @@
+#include "../include/Student.h"
+#include <iostream>
+#include <sstream>
 
+s24299::Student::Student(std::string x, std::string y, std::string z)
+    : first_name{std::move(x)}, last_name{std::move(y)}, index_number{std::move(z)} {
+}
+
+auto s24299::Student::to_string() const -> std::string
+{
+    auto result = std::ostringstream{};
+    result << first_name << " " << last_name << " (" << index_number << "), " 
+    << "Semester " << semester << ": " << grade_average;
+    
+    return result.str();
+}
+
+auto main() -> int
+{
+    auto student = s24299::Student("Jędrzej", "Stańczewski", "24299");
+    student.grade_average = 4.9;
+    std::cout << student.to_string() << std::endl;
+    
+    return 0;
+}
